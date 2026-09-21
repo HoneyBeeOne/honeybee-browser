b78269c240d0816b56eaf8eb625e1c0139fee561
Bimo _o <i.s41m0011@gmail.com>
2026-09-21 15:20:24 +0500

    feat(ui): add status bar (#24)
    Co-authored-by: isaim0011 <297376048+isaim0011@users.noreply.github.com>

--- src/browser/window.hpp
+++ src/browser/window.hpp
@@ -14,4 +14,7 @@ private:
     Gtk::Button m_reload_button;    // زر إعادة التحميل
     Gtk::Entry m_url_bar;           // شريط العنوان
     Gtk::Label m_label;             // النص المؤقت في وسط النافذة
+    Gtk::Box m_status_bar;          // شريط الحالة: تخطيط أفقي
+    Gtk::Label m_status_label;      // نص الحالة
+    Gtk::Label m_version_label;     // رقم الإصدار
 };
